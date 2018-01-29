b032284558d819a5ec2d3c743882a928f8f03fd5
Taisa Boiko <tayaboyko@gmail.com>
2018-01-29 08:02:59 +0200

    update(style): added fonts and scrollbar style

--- src/Gallery.vue
+++ src/Gallery.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-    <p>welcome to viseven imagestock</p>
-  </div>
+  <article id="app">
+    <div class="content">
+      <header id="galleryHeader">welcome to viseven <span class="highlighted">imagestock</span></header>
+      <div class="test">1</div>
+      <div class="test">2</div>
+      <div class="test">3</div>
+      <div class="test">4</div>
+      <div class="test">5</div>
+    </div>
+  </article>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'gallery',
   data () {
     return {
     }
@@ -15,5 +22,59 @@ export default {
 </script>
 
 <style lang="scss">
+  @import 'assets/style/reset.css';
+  @import 'assets/style/fonts.css';
 
+  #app {
+    width: 1024px;
+    height: 768px;
+    padding: 20px 20px 40px 20px;
+    box-sizing: border-box;
+    background: #f0f3f6;
+
+  }
+
+  #galleryHeader {
+    margin: 0 0 15px 5px;
+    color: #8499a7;
+    font: 800 24px OpenSans;
+    text-transform: uppercase;
+
+    .highlighted {
+      color: #21b8c6;
+    }
+  }
+
+  .content {
+    width: 100%;
+    height: 100%;
+    overflow-y: hidden;
+    overflow-x: auto;
+  }
+
+  .test {
+    width: 1500px;
+    height: 100px;
+    border: 2px red solid;
+    margin: 5px;
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+    border-radius: 7px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #d1dae3;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-button {
+    display: none;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #21b8c6;
+    border-radius: 10px;
+  }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,10 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
