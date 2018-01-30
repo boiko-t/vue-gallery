a85fdb66b479d4e58b22349c6b8b5bcd9aeb0fe1
Taisa Boiko <tayaboyko@gmail.com>
2018-01-30 09:41:27 +0200

    add(ImagePicker): new component

--- src/Gallery.vue
+++ src/Gallery.vue
@@ -1,80 +1,80 @@
 <template>
-  <article id="app">
-    <div class="content">
-      <header id="galleryHeader">welcome to viseven <span class="highlighted">imagestock</span></header>
-      <div class="test">1</div>
-      <div class="test">2</div>
-      <div class="test">3</div>
-      <div class="test">4</div>
-      <div class="test">5</div>
-    </div>
-  </article>
+    <article id="app">
+        <div class="content">
+            <header id="galleryHeader">welcome to viseven <span class="highlighted">imagestock</span></header>
+            <image-picker></image-picker>
+        </div>
+    </article>
 </template>
 
 <script>
-export default {
-  name: 'gallery',
-  data () {
-    return {
+    import ImagePicker from './components/ImgPicker.vue'
+
+    export default {
+        name: 'gallery',
+        components: {
+            ImagePicker
+        },
+        data () {
+            return {}
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-  @import 'assets/style/reset.css';
-  @import 'assets/style/fonts.css';
+    @import 'assets/style/main.scss';
 
-  #app {
-    width: 1024px;
-    height: 768px;
-    padding: 20px 20px 40px 20px;
-    box-sizing: border-box;
-    background: #f0f3f6;
+    #app {
+        width: 1024px;
+        height: 768px;
+        padding: 20px 20px 40px 20px;
+        box-sizing: border-box;
+        background: #f0f3f6;
 
-  }
-
-  #galleryHeader {
-    margin: 0 0 15px 5px;
-    color: #8499a7;
-    font: 800 24px OpenSans;
-    text-transform: uppercase;
-
-    .highlighted {
-      color: #21b8c6;
     }
-  }
 
-  .content {
-    width: 100%;
-    height: 100%;
-    overflow-y: hidden;
-    overflow-x: auto;
-  }
+    #galleryHeader {
+        margin: 0 0 15px 5px;
+        color: #8499a7;
+        font: 800 24px OpenSans;
+        text-transform: uppercase;
 
-  .test {
-    width: 1500px;
-    height: 100px;
-    border: 2px red solid;
-    margin: 5px;
-  }
+        .highlighted {
+            color: $blue;
+        }
+    }
 
-  ::-webkit-scrollbar {
-    width: 10px;
-    border-radius: 7px;
-  }
+    .content {
+        width: 100%;
+        height: 100%;
+        overflow-y: hidden;
+        overflow-x: auto;
+    }
 
-  ::-webkit-scrollbar-track {
-    background: #d1dae3;
-    border-radius: 10px;
-  }
+    .test {
+        width: 1500px;
+        height: 100px;
+        border: 2px red solid;
+        margin: 5px;
+    }
 
-  ::-webkit-scrollbar-button {
-    display: none;
-  }
+    ::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+        border-radius: 7px;
+    }
 
-  ::-webkit-scrollbar-thumb {
-    background: #21b8c6;
-    border-radius: 10px;
-  }
+    ::-webkit-scrollbar-track {
+        background: #d1dae3;
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-button {
+        display: none;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: $blue;
+        border-radius: 10px;
+    }
 </style>
---
+++ src/components/ImgPicker.vue
@@ -0,0 +1,55 @@
+<template>
+    <section id="imagePicker">
+        <div class="plus-sign-wrapper">
+            <img src="/src/assets/images/add-icon.png" alt="Add image">
+        </div>
+        <p class="caption">
+            Add your Picture
+        </p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'imagePicker',
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/style/main.scss';
+
+    #imagePicker {
+        @extend %gallery-element-shadow;
+        width: 235px;
+        height: 200px;
+        background: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        .plus-sign-wrapper {
+            width: 45px;
+            height: 45px;
+            background-color: $blue;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            & img {
+                width: 20px;
+                height: 20px;
+            }
+        }
+
+        .caption {
+            width: 100px;
+            text-align: center;
+        }
+    }
+</style>
