0405d037efc507525358a736fb602d7853377b5a
Taisa Boiko <tayaboyko@gmail.com>
2018-01-31 07:19:02 +0200

    add(GalleryItem): new component

---
+++ src/components/GalleryItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        galleryItem
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'galleryItem',
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/style/main.scss';
+</style>
